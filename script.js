92a0dd878f47f1d130df13e2f57cf4b82917fca9
gren210 <s10257926@connect.np.edu.sg>
2023-12-02 21:32:49 +0800

    Final changes

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-java
+let index = 1;
+theslides(index);
+
+// Function for controlling image
+function currentslide(n) {
+  theslides(index = n);
+  }
+
+// Function for the next and previous buttons
+function buttons(n) {
+theslides(index += n);
+}
+
+
+function theslides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slideshow"); // apply code to html code under this class
+  if (n > slides.length) {index = 1} // keep code valid
+  if (n < 1) {index = slides.length} // 
+  for (i = 0; i < slides.length; i++) { // loop through each image
+    slides[i].style.display = "none";
+  }
+  
+  slides[index-1].style.display = "block";
+}
